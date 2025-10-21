de2844606ff5e603a36dc49ca59ce27b1912843c
Stein-N <nico-stein-98@sbvl.de>
2025-10-21 17:13:44 +0200

    Finish BagCache

--- Bag/BagCache.lua
+++ Bag/BagCache.lua
@@ -4,18 +4,15 @@ BagCache = {}
 local _itemsByBag = {[0] = {}, [1] = {}, [2] = {}, [3] = {}, [4] = {}}
 
 -- Cache if Item is equipable
-local _equipableCache = setmetatable({}, {_mode = "k"})
+local _equipableCache = {}
 
 -- Cache Bag Sizes per Id
 local _bagSlots = {[0] = 0, [1] = 0, [2] = 0, [3] = 0, [4] = 0}
 
 -- Refresh the complete Player Bag Cache
-function BagCache:RefreshCache(container)
-    -- only refresh Cache if the Bag is actually combined
-    if not container == ContainerFrameCombinedBags then return end
-
+function BagCache:RefreshCache()
     BagCache:UpdateBagSlots()
-    BagCache:UpdateBagItems(container)
+    BagCache:UpdateBagItems()
 end
 
 -- Refresh the sizes of all Bags
@@ -26,17 +23,45 @@ function BagCache:UpdateBagSlots()
     end
 end
 
--- Collect all ItemButtons of the given Container
-function BagCache:UpdateBagItems(container)
-    -- Wipe all ItemButtons from the Cache
-    for bagId = 0, 4 do table.wipe(_itemsByBag[bagId]) end
+-- Collect ItemInfos
+function BagCache:UpdateBagItems()
+    -- Wipe equipable cache
+    table.wipe(_equipableCache)
 
-    for _, itemButton in container:EnumerateValidItems() do
-        local slot = itemButton:GetID()
-        _itemsByBag[itemButton.bagID][slot] = itemButton
+    for bagId = 0, 4 do
+        -- Wipe old data
+        table.wipe(_itemsByBag[bagId])
+        local slots = _bagSlots[bagId]
+
+        -- Collect all ItemInfos for the BagId
+        if slots > 0 then
+            for slot = 1, slots do
+                local itemInfo = C_Container.GetContainerItemInfo(bagId, slot)
+                if itemInfo and itemInfo.itemID then
+                    -- Save ItemInfo
+                    _itemsByBag[bagId][slot] = itemInfo
+                    local itemId = itemInfo.itemID
+
+                    -- Check if Item is equipable but only if the itemId wasnt cached before
+                    if _equipableCache[itemId] == nil then
+                        local _, _, _, equipLoc = C_Item.GetItemInfoInstant(itemId)
+                        local equipable = equipLoc ~= nil and equipLoc ~= "" and equipLoc ~= "INVTYPE_NON_EQUIP_IGNORE"
+                        _equipableCache[itemId] = equipable
+                    end
+                end
+            end
+        end
     end
 end
 
-function BagCache:UpdaetEquipable()
-    
+-- return ItemInfo
+function BagCache:GetItemInfo(bagId, slot)
+    if _itemsByBag[bagId] then
+        return _itemsByBag[bagId][slot]
+    else return nil end
+end
+
+-- check if ItemId is equipable
+function BagCache:IsEquipable(itemId)
+    return _equipableCache[itemId] or false
 end
