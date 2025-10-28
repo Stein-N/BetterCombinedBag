4ff806f334b79dcd6c91be70119a23acfbe30527
Stein-N <nico-stein-98@sbvl.de>
2025-10-28 16:32:50 +0100

    cleanup BagCore

--- BagCore.lua
+++ BagCore.lua
@@ -1,31 +1,22 @@
 BetterCombinedBagDB = {}
 
 local frame = CreateFrame("Frame")
-local handler = {}
-
-function handler.ADDON_LOADED(name)
-    if name == BagData.addonName then
-        BagMenu:BuildOptionsMenu()
-        BagUtils:UpdateSettings()
-    end
-end
-
-function handler.PLAYER_ENTERING_WORLD()
-    -- refresh BagCache
-    BagCache:RefreshCache()
-end
-
-function handler:BAG_UPDATE_DELAYED()
-    BagCache:RefreshCache()
-end
 
 frame:RegisterEvent("ADDON_LOADED")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 frame:RegisterEvent("BAG_UPDATE_DELAYED")
 
-frame:SetScript("OnEvent", function(self, event, ...)
-    local func = handler[event]
-    if func then return func(...) end
+frame:SetScript("OnEvent", function(self, event, name)
+    if event == "PLAYER_ENTERING_WORLD" or event == "BAG_UPDATE_DELAYED" then
+        BagCache:RefreshCache()
+    end
+
+    if event == "ADDON_LOADED" then
+        if name == BagData.addonName then
+            BagMenu:BuildOptionsMenu()
+            BagUtils:UpdateSettings()
+        end
+    end
 end)
 
 ---##################################---
