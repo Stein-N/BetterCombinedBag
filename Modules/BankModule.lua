ab7a59edba26e356df5ced10cf4ec6116efb40d3
Stein-N <nico-stein-98@sbvl.de>
2026-01-26 19:06:01 +0100

    fix BankModule - fix ItemLevel Display - fix TabButton Position - increase performance a bit

--- Modules/BankModule.lua
+++ Modules/BankModule.lua
@@ -3,6 +3,10 @@ local _, addon = ...
 local borderPad, itemPad =  7, 4
 local btnSize = nil
 
+local tabButtons = {}
+
+-- TODO: Needs a big cleanup
+
 local tabButton = CreateFrame("Button", "BetterCombinedBagEverythingTabButton", BankFrame.BankPanel, "BankPanelTabTemplate")
 tabButton:Init({ IsPurchaseTab = false, ID = 99 })
 tabButton.Icon:SetTexture("interface/icons/ability_bossashvane_icon03")
@@ -24,25 +28,29 @@ local function ResizeBankFrame(panel, width, height)
     panel:SetSize(width, height)
 end
 
+local function UpdateBankItemButton(tabId, slot, btn)
+    local info = addon.ItemInfoCache[tabId][slot]
+    if info ~= nil and info.hyperlink ~= nil then
+        local itemLevel = addon.GetItemLevelFromItemLink(info.hyperlink)
+        local item = Item:CreateFromItemLink(info.hyperlink)
+
+        if itemLevel ~= nil and item ~= nil then
+            addon.UpdateItemLevelComponent(btn, itemLevel, item:GetItemQuality())
+        else
+            btn.ItemLevelComponent:Hide()
+        end
+    else
+        btn.ItemLevelComponent:Hide()
+    end
+end
+
 local function UpdateBaseLayout(panel)
     local x, y, counter = -borderPad, 70, 0
     for btn in panel:EnumerateValidItems() do
         if btnSize == nil then btnSize = btn:GetWidth() end
         addon.AddItemLevelComponent(btn)
 
-        local info = addon.ItemInfoCache[panel:GetSelectedTabID()][btn:GetContainerSlotID()]
-        if info ~= nil and info.hyperlink ~= nil then
-            local itemLevel = addon.GetItemLevelFromItemLink(info.hyperlink)
-            local item = Item:CreateFromItemLink(info.hyperlink)
-
-            if itemLevel ~= nil and item ~= nil then
-                addon.UpdateItemLevelComponent(btn, itemLevel, item:GetItemQuality())
-            else
-                btn.ItemLevelComponent:Hide()
-            end
-        else
-            btn.ItemLevelComponent:Hide()
-        end
+        UpdateBankItemButton(panel:GetSelectedTabID(), btn:GetContainerSlotID(), btn)
 
         btn:ClearAllPoints()
         btn:SetParent(panel)
@@ -64,12 +72,12 @@ local function UpdateBaseLayout(panel)
     ResizeBankFrame(panel, width, height)
 end
 
-local function UpdateEverythingTab(panel, firstBag, lastBag)
+local function UpdateEverythingTab(panel, first, last)
     local x, y, counter = borderPad, -60, 0
     local columns = 33
     local rows = 0
 
-    for bagId = firstBag, lastBag do
+    for bagId = first, last do
         if C_Container.GetContainerNumSlots(bagId) ~= 0 then
             local buttons = addon.CustomBagButtons[bagId]
             rows = rows + math.ceil(98 / columns)
@@ -105,6 +113,32 @@ local function UpdateEverythingTab(panel, firstBag, lastBag)
     ResizeBankFrame(panel, width, height)
 end
 
+local function CollectTabButtons(panel)
+    for _, btn in ipairs({ panel:GetChildren() }) do
+        if btn.IsPurchaseTab ~= nil then
+            tabButtons[btn.tabData.ID] = btn
+        end
+    end
+end
+
+local function UpdateTabButtonPosition(panel, first, last)
+    if panel == nil then return end
+    local x, y = 3, -30
+
+    tabButton:ClearAllPoints()
+    tabButton:SetPoint("TOPLEFT", panel, "TOPRIGHT", x, y)
+    y = y - (tabButton:GetHeight() + 15)
+
+    for i = first, last do
+        local btn = tabButtons[i]
+        if btn ~= nil then
+            btn:ClearAllPoints()
+            btn:SetPoint("TOPLEFT", panel, "TOPRIGHT", x, y)
+            y = y - (btn:GetHeight() + 15)
+        end
+    end
+end
+
 local function HideCustomButtons()
     for i = 6, 17 do
         local buttons = addon.CustomBagButtons[i]
@@ -117,37 +151,37 @@ end
 ------------------------------------------
 hooksecurefunc(BankFrame.BankPanel, "GenerateItemSlotsForSelectedTab", function(self)
     addon.CacheItemInfos()
-
-    local index = 1
-    for _, btn in ipairs({ self.bankTabPool }) do
-        index = index + 1
-    end
-
-    print("Found", index, "Tab Buttons")
+    CollectTabButtons(self)
 
     local tabId = self:GetSelectedTabID()
-    local bankType = self.bankType
 
     borderPad = BCB_Settings.bankBorderPadding + 7
     itemPad = BCB_Settings.bankItemPadding + 4
 
-    if tabId ~= 99 then
-        UpdateBaseLayout(self)
-        HideCustomButtons()
-    end
+    local config = {
+        [0] = { first = 6,  last = 11 },
+        [2] = { first = 12, last = 16 }
+    }
 
-    if tabId == 99 then
-        if bankType == 0 then
-            UpdateEverythingTab(self, 6, 11)
-        elseif bankType == 2 then
-            UpdateEverythingTab(self, 12, 16)
+    local settings = config[self.bankType]
+
+    if settings ~= nil then
+        if tabId == 99 then
+            UpdateEverythingTab(self, settings.first, settings.last)
+            UpdateTabButtonPosition(self, settings.first, settings.last)
+        else
+            UpdateBaseLayout(self)
+            HideCustomButtons()
+            UpdateTabButtonPosition(self, settings.first, settings.last)
         end
     end
 end)
 
-hooksecurefunc(BankFrame.BankPanel, "ShowPurchasePrompt", HideCustomButtons)
-
-hooksecurefunc(BankFrame, "Show", function(self)
-    tabButton:SetPoint("LEFT", self.BankPanel, "RIGHT", 2, -175)
-    tabButton:Show()
+hooksecurefunc(BankFrame.BankPanel, "MarkDirty", function(self)
+    addon.CacheItemInfos()
+    for btn in self:EnumerateValidItems() do
+        UpdateBankItemButton(self:GetSelectedTabID(), btn:GetContainerSlotID(), btn)
+    end
 end)
+
+hooksecurefunc(BankFrame.BankPanel, "ShowPurchasePrompt", HideCustomButtons)
